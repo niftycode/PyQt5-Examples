3b24c7bc127157cd17bb9f7446cf7318c9e29296
Bodo Schönfeld <bodo.schoenfeld@niftycode.de>
2020-03-15 10:27:45 +0100

    add 1st example (pyqt5_window.py)

---
+++ Basics/pyqt5_window.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Qt5 Window
+
+Author: niftycode
+Date created: March 15th, 2020
+"""
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+
+    # Create a window
+    window = QWidget()
+    window.resize(450, 300)
+    window.move(300, 300)
+    window.setWindowTitle('Qt5-Window')
+    window.show()
+
+    sys.exit(app.exec_())
